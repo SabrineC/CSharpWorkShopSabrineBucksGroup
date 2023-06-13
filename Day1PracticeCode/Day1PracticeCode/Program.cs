530e05156a14e56d000d5c5c3a61152bdd02b04a
Sabrine <91069477+SabrineC@users.noreply.github.com>
2023-06-13 12:17:27 +0100

    Day1
    Practiced coding with a console application, making different types of variables and outputting them.

---
+++ Day1PracticeCode/Day1PracticeCode/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+//create a string variable
+string myName = "Sabrine";
+
+//create another variable
+string superHeroName = "The Flash";
+string secondMovieName = "AdiPurush";
+
+//building the output string
+//+ is the concatenaton operator
+string outputString = "This Thursday Jay will be watching the movie, " + superHeroName;
+Console.WriteLine(outputString);
+string outputString2 = "This Friday Jay will be watching the movie, " + secondMovieName;
+Console.WriteLine(outputString2);
+
+//only characters use single quotes
+char firstLetter = 'a';
+//string testingTheory = 'a';
+
+//display a variable
+//Console.WriteLine(superHeroName);
+
+//create a variable to store an integer
+int myAge = 18;
+//Console.WriteLine(myAge);
+
+//create a decimal variable
+decimal myMoney = 100.50m;
+//Console.WriteLine(myMoney);
+
+//create a double variable
+double myDouble = 100.5;
+//Console.WriteLine(myDouble);
+
+//create a boolean variable
+bool isRaining = true;
+//Console.WriteLine(isRaining);
