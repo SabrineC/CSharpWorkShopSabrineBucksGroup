6b75df683aa49ddf9e376cf2e842dedaa0cf9868
Sabrine <91069477+SabrineC@users.noreply.github.com>
2023-06-15 20:02:20 +0100

    Day1 Homework
    Homework for Day 1

---
+++ Day1Homework/Day1Homework/Program.cs
@@ -0,0 +1,30 @@
+﻿//Create a project that has the following things
+//At least 2 string variables
+//At least 2 integer variables
+//At least 2 double/decimal variables
+//Build a string that combines at least 2 strings
+//Show all these variables in a output.
+
+//string variables
+string string1 = "Hello ";
+string string2 = "I'm Sabrine";
+
+//integer variables
+int integer1 = 1;
+int integer2 = 2;
+
+//decimal variables
+decimal decimal1 = 9.99m;
+decimal decimal2 = 100.00m;
+
+//variable that is a combination of 2 strings
+string combine = string1 + string2;
+
+//output variables
+Console.WriteLine(string1);
+Console.WriteLine(string2);
+Console.WriteLine(combine);
+Console.WriteLine(integer1);
+Console.WriteLine(integer2);
+Console.WriteLine(decimal1);
+Console.WriteLine(decimal2);
