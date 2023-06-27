6901c4b5ab1e647c92b5e0d962faf5df96feb9af
Sabrine <91069477+SabrineC@users.noreply.github.com>
2023-06-27 12:06:26 +0100

    Day5
    Building functions within classes itself and displaying them

---
+++ StudentDeveloper/StudentDeveloper/Program.cs
@@ -0,0 +1,61 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using StudentDeveloper;
+
+// Student called Bruce Wayne
+Student BruceWayne = new Student();
+// Student called Selina Kyle
+Student SelinaKyle = new Student();
+
+BruceWayne.NameOfStudent = "Bruce Wayne";
+BruceWayne.StudyHoursPerWeek = 20;
+BruceWayne.StudyWeeksCompleted = 0;
+
+BruceWayne.BuildStudentInformation();
+BruceWayne.DisplayStudentInformation();
+
+SelinaKyle.NameOfStudent = "Selina Kyle";
+SelinaKyle.StudyHoursPerWeek = 10;
+SelinaKyle.StudyWeeksCompleted = 10;
+
+SelinaKyle.BuildStudentInformation();
+SelinaKyle.DisplayStudentInformation();
+
+#region
+//string outputForBatman = $"Student Name: {BruceWayne.NameOfStudent}" +
+//    $" Study Hours: {BruceWayne.StudyHoursPerWeek}" +
+//    $" Number of Weeks Completed: {BruceWayne.StudyWeeksCompleted}";
+//string outputForCatwoman = $"Student Name: {SelinaKyle.NameOfStudent} " +
+//    $" Study Hours: {SelinaKyle.StudyHoursPerWeek}" +
+//    $" Number of Weeks Completed: {SelinaKyle.StudyWeeksCompleted}";
+
+//Console.WriteLine(outputForBatman);
+//Console.WriteLine(outputForCatwoman);
+
+#endregion
+
+// Super Hero called The Phantom
+Student KitWalker = new Student();
+KitWalker.NameOfStudent = "Kit Walker";
+KitWalker.StudyHoursPerWeek = 30;
+KitWalker.StudyWeeksCompleted = 30;
+
+KitWalker.BuildStudentInformation();
+KitWalker.DisplayStudentInformation();
+
+// 500 hours is the amount of time it takes to become
+// a beginner developer
+
+int TotalNumberOfHoursToBecomeDeveloper = 500;
+
+// 20 hours per week
+// 25 weeks to become a beginner developer
+// Number of weeks already completed is 0
+// Remaining weeks = 25 - 0 = 25 weeks
+// Ultimatley, Batman will become a developer in 25 weeks
+
+// 10 hours per week
+// 50 weeks to become a beginner developer
+// Number of weeks already completed is 10
+// Remaining weeks = 50 - 10 = 40 weeks
+// Eventually, Catwoman will become a develoepr in 40 weeks
---
+++ StudentDeveloper/StudentDeveloper/Student.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace StudentDeveloper
+{
+    public class Student
+    {
+        // Fields or Properties or Variables 
+        public int StudyHoursPerWeek { get; set; }
+        public int StudyWeeksCompleted { get; set; }
+        public string NameOfStudent { get; set; }
+
+        // For the output
+        public string OutputStorage { get; set; }   
+
+        // Functions or Methods or Behaviours or Actions
+        public void BuildStudentInformation()
+        {
+            this.OutputStorage = $"Student Name: {this.NameOfStudent} " +
+                $"Study Hours: {this.StudyHoursPerWeek}" +
+                $" Number of Weeks Completed: {this.StudyWeeksCompleted}";
+        }
+
+        public void DisplayStudentInformation()
+        {
+            Console.WriteLine(this.OutputStorage);
+        }
+    }
+}
